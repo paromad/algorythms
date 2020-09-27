c95b40801dcfb8aa62369988a4ca65d98987a9c1
paromad <luba100901@mail.ru>
2020-09-28 00:34:04 +0300

    fix H

--- h.cpp
+++ h.cpp
@@ -7,40 +7,50 @@
 using namespace std;
 
 class UnorderedSet {
-    static const size_t index_ = 2;
-    vector<list<string>> arr_;
+    static const size_t start_size_ = 100;
+    static const size_t index_of_expansion = 2;
+    vector<list<string>> strings_;
     size_t size_;
-public:
-    UnorderedSet() : arr_(vector<list<string>>(100)), size_(0) {};
 
-    void ChangeSize() {
-        auto tmp = arr_;
-        arr_ = vector<list<string>>(size_ * index_);
-        for (const auto &i : tmp) {
+    void InsertElementsFromVector(const vector<list<string>> &strings) {
+        for (const auto &i : strings) {
             for (const auto &j : i) {
                 Insert(j);
             }
         }
+    }
+
+    size_t Hash(std::string x) {
+        return std::hash<string>{}(x) % strings_.size();
+    }
+
+public:
+    UnorderedSet() : strings_(vector<list<string>>(start_size_)), size_(0) {};
+
+    void ChangeSize() {
+        auto tmp = strings_;
+        strings_ = vector<list<string>>(size_ * index_of_expansion);
+        InsertElementsFromVector(tmp);
     };
 
     void Insert(const string &x) {
         ++size_;
-        if (size_ == arr_.size()) {
+        if (size_ == strings_.size()) {
             ChangeSize();
         }
-        size_t hash = std::hash<string>{}(x) % arr_.size();
-        arr_[hash].push_back(x);
+        size_t hash = Hash(x);
+        strings_[hash].push_back(x);
     }
 
     void Erase(const string &x) {
-        size_t hash = std::hash<string>{}(x) % arr_.size();
-        arr_[hash].erase(std::find(arr_[hash].begin(), arr_[hash].end(), x));
+        size_t hash = Hash(x);
+        strings_[hash].erase(std::find(strings_[hash].begin(), strings_[hash].end(), x));
         --size_;
     }
 
     bool Find(const string &x) {
-        size_t hash = std::hash<string>{}(x) % arr_.size();
-        return std::find(arr_[hash].begin(), arr_[hash].end(), x) != arr_[hash].end() ? true : false;
+        size_t hash = Hash(x);
+        return std::find(strings_[hash].begin(), strings_[hash].end(), x) != strings_[hash].end();
     }
 };
 
