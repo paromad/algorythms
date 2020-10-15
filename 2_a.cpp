cc07bde8d86848aeb18a53a9a9bc6357296ad6e4
paromad <luba100901@mail.ru>
2020-10-15 23:41:31 +0300

    add class Graph

--- 2_a.cpp
+++ 2_a.cpp
@@ -4,15 +4,82 @@
 
 using namespace std;
 
-template<typename T>
-void BFS(const vector<vector<T>> &graph, T start, vector<int> &distance, vector<T> &parents) {
-    queue<T> que;
+typedef size_t Vertex;
+
+class Graph {
+protected:
+    size_t vertex_count_;
+    size_t edge_count_;
+    bool is_directed_;
+public:
+    Graph(size_t vertex_count, size_t edje_count, bool is_directed) : vertex_count_(vertex_count),
+                                                                      edge_count_(edje_count),
+                                                                      is_directed_(is_directed) {};
+
+    size_t get_vertex_count() const {
+        return vertex_count_;
+    };
+
+    size_t ger_edge_count() const {
+        return edge_count_;
+    };
+
+    bool is_directed() const {
+        return is_directed_;
+    }
+
+    virtual void add_edge(const Vertex &start, const Vertex &finish) = 0;
+
+    virtual std::vector<Vertex> get_neighbours(Vertex v) const = 0;
+
+    virtual std::vector<Vertex> get_shortest_path(const Vertex &v, size_t dist, const vector<Vertex> &parent) const = 0;
+};
+
+
+class AdjListGraph : public Graph {
+    vector<vector<Vertex>> adj_list_;
+public:
+    AdjListGraph(size_t vertex_count, size_t edge_count, bool is_directed) : Graph(vertex_count, edge_count,
+                                                                                   is_directed) {
+        adj_list_.resize(vertex_count + 1);
+    };
+
+    void add_edge(const Vertex &start, const Vertex &finish) override {
+        adj_list_[start].push_back(finish);
+        if (!is_directed()) {
+            adj_list_[finish].push_back(start);
+        }
+    }
+
+    std::vector<Vertex> get_neighbours(Vertex v) const override {
+        return adj_list_[v];
+    }
+
+    std::vector<Vertex> get_shortest_path(const Vertex &v, size_t dist, const vector<Vertex> &parent) const override {
+        std::vector<Vertex> res;
+        if (dist == vertex_count_ + 1) {
+            return res;
+        }
+        res.resize(dist + 1);
+        Vertex finish = v;
+        res[dist] = finish;
+        for (size_t i = 1; i <= dist; ++i) {
+            finish = parent[finish];
+            res[dist - i] = finish;
+        }
+        return res;
+    }
+};
+
+
+void BFS(const Graph &graph, Vertex start, vector<size_t> &distance, vector<Vertex> &parents) {
+    queue<Vertex> que;
     que.push(start);
     distance[start] = 0;
     while (!que.empty()) {
-        int v = que.front();
+        Vertex v = que.front();
         que.pop();
-        for (size_t u : graph[v]) {
+        for (Vertex u : graph.get_neighbours(v)) {
             if (distance[u] > distance[v] + 1) {
                 distance[u] = distance[v] + 1;
                 parents[u] = v;
@@ -22,48 +89,43 @@ void BFS(const vector<vector<T>> &graph, T start, vector<int> &distance, vector<
     }
 }
 
-int main() {
-    size_t n, m, start, finish;
 
-    cin >> n >> m;
+int main() {
+    size_t vertex_count, edge_count;
+    Vertex start, finish;
+
+    cin >> vertex_count >> edge_count;
     cin >> start >> finish;
 
-    --start;
-    --finish;
+    start;
+    finish;
 
-    vector<vector<int>> graph(n);
+    bool is_directed = false;
 
-    for (size_t i = 0; i < m; ++i) {
+    AdjListGraph graph(vertex_count, edge_count, is_directed);
+
+    for (size_t i = 0; i < edge_count; ++i) {
         int first, second;
         cin >> first >> second;
-        graph[first - 1].push_back(second - 1);
-        graph[second - 1].push_back(first - 1);
+        graph.add_edge(first, second);
     }
 
-    vector<int> distance(n, n + 1);
-    vector<int> parents(n, -1);
+    vector<size_t> distance(vertex_count + 1, vertex_count + 1);
+    vector<Vertex> parent(vertex_count + 1);
 
-    BFS<int>(graph, start, distance, parents);
+    BFS(graph, start, distance, parent);
 
-    int dist = distance[finish];
-    if (dist == n + 1) {
-        cout << "-1";
+    vector<Vertex> res = graph.get_shortest_path(finish, distance[finish], parent);
+
+    if (!res.size()) {
+        cout << -1;
         return 0;
     }
 
-    cout << dist << "\n";
+    cout << res.size() - 1 << "\n";
 
-    vector<int> res(dist + 1);
-
-    res[dist--] = finish;
-
-    while (dist >= 0) {
-        finish = parents[finish];
-        res[dist--] = finish;
-    }
-
-    for (int i : res) {
-        cout << i + 1 << " ";
+    for (Vertex i : res) {
+        cout << i << " ";
     }
 
     return 0;
