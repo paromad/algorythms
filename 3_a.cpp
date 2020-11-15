600670d91088af5d9d7ac7f2d17efc5537568579
paromad <luba100901@mail.ru>
2020-11-15 13:42:33 +0300

    add DFSData

--- 3_a.cpp
+++ 3_a.cpp
@@ -55,23 +55,36 @@ public:
     }
 };
 
-void DFS(const Graph &graph, const Vertex &v, bool is_root, vector<bool> &used, size_t &time, vector<size_t> &time_in,
-         vector<size_t> &time_up, vector<Vertex> &parent, vector<Vertex> &cut_vertex) {
-    used[v] = true;
-    time_in[v] = time_up[v] = ++time;
+struct DFSData {
+    vector<bool> used;
+    vector<size_t> time_in;
+    vector<size_t> time_up;
+    vector<Vertex> parent;
+
+    DFSData(size_t size) {
+        vector<bool> used(size, false);
+        vector<size_t> time_in(size);
+        vector<size_t> time_up(size);
+        vector<Vertex> parent(size, 0);
+    }
+};
+
+void DFS(const Graph &graph, const Vertex &v, bool is_root, size_t &time, DFSData &data, vector<Vertex> &cut_vertex) {
+    data.used[v] = true;
+    data.time_in[v] = data.time_up[v] = ++time;
     size_t n_children = 0;
     bool is_cut_vertex = false;
     for (Vertex u : graph.get_neighbours(v)) {
-        if (u != parent[v]) {
-            if (used[u]) {
-                time_up[v] = std::min(time_up[v], time_in[u]);
+        if (u != data.parent[v]) {
+            if (data.used[u]) {
+                data.time_up[v] = std::min(data.time_up[v], data.time_in[u]);
             }
             else {
-                parent[u] = v;
-                DFS(graph, u, false, used, time, time_in, time_up, parent, cut_vertex);
+                data.parent[u] = v;
+                DFS(graph, u, false, time, data, cut_vertex);
                 ++n_children;
-                time_up[v] = std::min(time_up[v], time_up[u]);
-                if (time_in[v] <= time_up[u] && !is_root) {
+                data.time_up[v] = std::min(data.time_up[v], data.time_up[u]);
+                if (data.time_in[v] <= data.time_up[u] && !is_root) {
                     is_cut_vertex = true;
                 }
             }
@@ -84,14 +97,11 @@ void DFS(const Graph &graph, const Vertex &v, bool is_root, vector<bool> &used, 
 
 vector<Vertex> CutVertexes(const Graph &graph) {
     vector<Vertex> cut_vertexes;
-    vector<bool> used(graph.get_vertex_count() + 1, false);
-    vector<size_t> time_in(graph.get_vertex_count() + 1);
-    vector<size_t> time_up(graph.get_vertex_count() + 1);
-    vector<Vertex> parent(graph.get_vertex_count() + 1, 0);
+    DFSData data(graph.get_vertex_count() + 1);
     size_t time = 0;
     for (size_t v = 1; v <= graph.get_vertex_count(); ++v) {
-        if (!used[v]) {
-            DFS(graph, v, true, used, time, time_in, time_up, parent, cut_vertexes);
+        if (!data.used[v]) {
+            DFS(graph, v, true, time, data, cut_vertexes);
         }
     }
     return cut_vertexes;
@@ -111,7 +121,7 @@ int main() {
     AdjListGraph graph(vertex_count, edge_count, is_directed);
 
     for (size_t i = 0; i < edge_count; ++i) {
-        int first, second;
+        Vertex first, second;
         cin >> first >> second;
         graph.add_edge(first, second);
     }
@@ -122,8 +132,8 @@ int main() {
 
     std::sort(res.begin(), res.end());
 
-    for (auto it = res.begin(); it != res.end(); ++it) {
-        cout << *it << "\n";
+    for (const Vertex &vertex : res) {
+        cout << vertex << "\n";
     }
 
     return 0;
